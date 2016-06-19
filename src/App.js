8b898d91b47408d78dfc08404ae7921321d0e42d
alejodiazg <09-10234@usb.ve>
2016-06-19 14:36:19 -0400

    test for bundle

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Layout from './Layout';
 import Counter from './Counter';
+import Header from './Header';
 
 export default class App extends Component {
   render() {
     return (
       <Layout>
         <Counter />
+        <Header />
       </Layout>
     );
   }
